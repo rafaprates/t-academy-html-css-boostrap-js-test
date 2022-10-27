47cf25abe468d20ad53c057192f4238eea40264f
Rafael Cardoso <rafaelbprates@gmail.com>
2022-10-27 10:42:22 -0300

    [add] incrementa quantidade de produto em carrinho

--- main.js
+++ main.js
@@ -1,43 +1,5 @@
-var segmentos = [
-  { nome: "telefonia" },
-  { nome: "vestuario" },
-  { nome: "alimetício" },
-];
-
-var produtos = [
-  { nome: "iPhone 4", segmento: "telefonia", preco: 1000, estoque: 100 },
-  { nome: "iPhone 4s", segmento: "telefonia", preco: 1050, estoque: 100 },
-  { nome: "iPhone 5", segmento: "telefonia", preco: 1100, estoque: 100 },
-  { nome: "iPhone 5s", segmento: "telefonia", preco: 1250, estoque: 100 },
-  { nome: "iPhone 6", segmento: "telefonia", preco: 1200, estoque: 100 },
-  { nome: "iPhone 6s", segmento: "telefonia", preco: 1250, estoque: 100 },
-  { nome: "iPhone 7", segmento: "telefonia", preco: 1400, estoque: 100 },
-  { nome: "iPhone 7s", segmento: "telefonia", preco: 1500, estoque: 100 },
-  { nome: "iPhone 12", segmento: "telefonia", preco: 3000, estoque: 100 },
-  { nome: "iPhone 12 Pro", segmento: "telefonia", preco: 3800, estoque: 100 },
-
-  { nome: "camiseta", segmento: "vestuario", preco: 249, estoque: 100 },
-  { nome: "bandeira", segmento: "vestuario", preco: 100, estoque: 100 },
-  { nome: "short ", segmento: "vestuario", preco: 59, estoque: 100 },
-  { nome: "tenis ", segmento: "vestuario", preco: 300, estoque: 100 },
-  { nome: "chinelo ", segmento: "vestuario", preco: 39, estoque: 100 },
-  { nome: "camiseta regata ", segmento: "vestuario", preco: 99, estoque: 100 },
-  { nome: "calça ", segmento: "vestuario", preco: 199, estoque: 100 },
-  { nome: "boné ", segmento: "vestuario", preco: 79, estoque: 100 },
-  { nome: "cinto ", segmento: "vestuario", preco: 89, estoque: 100 },
-  { nome: "sunga ", segmento: "vestuario", preco: 69, estoque: 100 },
-];
-
-var carrinho = [];
-
-console.log("INCIALIZOU");
-
 inicializarLocalStorage();
 carregarArraysComLocalStorage();
-
-function existeSegmento() {}
-function validarSegmento() {}
-function excluirSegmento() {}
 
 function atualizarTabelaSegmentos() {
   var dados = "";
@@ -61,9 +23,9 @@ function atualizarTabelaProdutos(tabelaProdutos = produtos) {
                 <td>${p.segmento}</td>
                 <td>${p.estoque}</td>
                 <td>R$ ${p.preco}</td>
-                <td><button onclick="excluirMarca(${produtos.indexOf(
+                <td><button onclick="addCarrinho(${produtos.indexOf(
                   p
-                )})" class="btn btn-outline-danger btn-sm">Excluir</button></td>
+                )})" class="btn btn-outline-info btn-sm">Carrinho</button></td>
                 </tr>`;
 
     document.getElementsByTagName("tbody")[0].innerHTML = dados;
@@ -104,6 +66,65 @@ function pesquisarProduto() {
   atualizarTabelaProdutos(produtosComTermo);
 }
 
+function addCarrinho(idProduto) {
+  console.log("addCarrinho()");
+  // encontrar produto no array de Produtos
+  let p = produtos[idProduto];
+  console.log(p);
+
+  // descontar a quantidade do estoque
+  // atualizar localStorage
+  p.estoque = p.estoque - 1;
+  localStorage.setItem("produtos", JSON.stringify(produtos));
+
+  // Construir objeto que vai para carrinho
+  if (existeProdutoEmCarrinho(p.nome)) {
+    c = carrinho.find((element, index) => {
+      return element.nomeProduto === p.nome;
+    });
+    c.quantidade = c.quantidade + 1;
+  } else {
+    c = {
+      nomeProduto: p.nome,
+      segmentoProduto: p.segmento,
+      precoProduto: p.preco,
+      quantidade: 1,
+      precoProduto: p.preco,
+    };
+    carrinho.push(c);
+  }
+
+  // adicionar produto ao carrinho com o estoque descontado
+
+  localStorage.setItem("carrinho", JSON.stringify(carrinho));
+}
+
+function atualizarTabelaCarrinho() {
+  var dados = "";
+  for (c of carrinho) {
+    dados += `<tr>
+                <td>${c.nomeProduto}</td>
+                <td>${c.segmentoProduto}</td>
+                <td>R$ ${c.precoProduto}</td>
+                <td>${c.quantidade}</td>
+                <td><button onclick="excluirProdutoCarrinho(${carrinho.indexOf(
+                  c
+                )})" class="btn btn-outline-danger btn-sm">Excluir</button></td>
+                </tr>`;
+
+    document.getElementsByTagName("tbody")[0].innerHTML = dados;
+  }
+}
+
+function existeProdutoEmCarrinho(nomeProduto) {
+  for (c of carrinho) {
+    if (c.nomeProduto == nomeProduto) {
+      return true;
+    }
+  }
+  return false;
+}
+
 function carregarArraysComLocalStorage() {
   if (localStorage.getItem("segmentos") != null) {
     segmentos = JSON.parse(localStorage.getItem("segmentos"));
@@ -119,18 +140,58 @@ function carregarArraysComLocalStorage() {
 }
 
 function inicializarLocalStorage() {
-  localStorage.setItem("segmentos", JSON.stringify(segmentos));
-  localStorage.setItem("produtos", JSON.stringify(produtos));
-  localStorage.setItem("carrinho", JSON.stringify(carrinho));
-  // if (localStorage.getItem("segmentos") == null) {
   // localStorage.setItem("segmentos", JSON.stringify(segmentos));
-  // }
-
-  // if (localStorage.getItem("produtos") == null) {
   // localStorage.setItem("produtos", JSON.stringify(produtos));
-  // }
-
-  // if (localStorage.getItem("carrinho") == null) {
   // localStorage.setItem("carrinho", JSON.stringify(carrinho));
-  // }
+  var segmentos = [
+    { nome: "telefonia" },
+    { nome: "vestuario" },
+    { nome: "alimetício" },
+  ];
+
+  if (localStorage.getItem("segmentos") == null) {
+    localStorage.setItem("segmentos", JSON.stringify(segmentos));
+  }
+
+  if (localStorage.getItem("produtos") == null) {
+    var produtos = [
+      { nome: "iPhone 4", segmento: "telefonia", preco: 1000, estoque: 100 },
+      { nome: "iPhone 4s", segmento: "telefonia", preco: 1050, estoque: 100 },
+      { nome: "iPhone 5", segmento: "telefonia", preco: 1100, estoque: 100 },
+      { nome: "iPhone 5s", segmento: "telefonia", preco: 1250, estoque: 100 },
+      { nome: "iPhone 6", segmento: "telefonia", preco: 1200, estoque: 100 },
+      { nome: "iPhone 6s", segmento: "telefonia", preco: 1250, estoque: 100 },
+      { nome: "iPhone 7", segmento: "telefonia", preco: 1400, estoque: 100 },
+      { nome: "iPhone 7s", segmento: "telefonia", preco: 1500, estoque: 100 },
+      { nome: "iPhone 12", segmento: "telefonia", preco: 3000, estoque: 100 },
+      {
+        nome: "iPhone 12 Pro",
+        segmento: "telefonia",
+        preco: 3800,
+        estoque: 100,
+      },
+
+      { nome: "camiseta", segmento: "vestuario", preco: 249, estoque: 100 },
+      { nome: "bandeira", segmento: "vestuario", preco: 100, estoque: 100 },
+      { nome: "short ", segmento: "vestuario", preco: 59, estoque: 100 },
+      { nome: "tenis ", segmento: "vestuario", preco: 300, estoque: 100 },
+      { nome: "chinelo ", segmento: "vestuario", preco: 39, estoque: 100 },
+      {
+        nome: "camiseta regata ",
+        segmento: "vestuario",
+        preco: 99,
+        estoque: 100,
+      },
+      { nome: "calça ", segmento: "vestuario", preco: 199, estoque: 100 },
+      { nome: "boné ", segmento: "vestuario", preco: 79, estoque: 100 },
+      { nome: "cinto ", segmento: "vestuario", preco: 89, estoque: 100 },
+      { nome: "sunga ", segmento: "vestuario", preco: 69, estoque: 100 },
+    ];
+    localStorage.setItem("produtos", JSON.stringify(produtos));
+  }
+
+  if (localStorage.getItem("carrinho") == null) {
+    var carrinho = [];
+    localStorage.setItem("carrinho", JSON.stringify(carrinho));
+  }
 }
