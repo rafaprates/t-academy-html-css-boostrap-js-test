f82fe2634a3ee14aa265c8471bf4b8d3c41022d2
Rafael Cardoso <rafaelbprates@gmail.com>
2022-10-27 09:04:18 -0300

    [removed] páginas de cadastro

--- main.js
+++ main.js
@@ -1,39 +1,75 @@
-var marcas = [];
-var segmentos = [];
+var segmentos = [
+  { nome: "telefonia" },
+  { nome: "vestuario" },
+  { nome: "alimetício" },
+];
+
+var produtos = [
+  { nome: "iPhone 4", segmento: "telefonia", preco: 1000, estoque: 100 },
+  { nome: "iPhone 4s", segmento: "telefonia", preco: 1050, estoque: 100 },
+  { nome: "iPhone 5", segmento: "telefonia", preco: 1100, estoque: 100 },
+  { nome: "iPhone 5s", segmento: "telefonia", preco: 1250, estoque: 100 },
+  { nome: "iPhone 6", segmento: "telefonia", preco: 1200, estoque: 100 },
+  { nome: "iPhone 6s", segmento: "telefonia", preco: 1250, estoque: 100 },
+  { nome: "iPhone 7", segmento: "telefonia", preco: 1400, estoque: 100 },
+  { nome: "iPhone 7s", segmento: "telefonia", preco: 1500, estoque: 100 },
+  { nome: "iPhone 12", segmento: "telefonia", preco: 3000, estoque: 100 },
+  { nome: "iPhone 12 Pro", segmento: "telefonia", preco: 3800, estoque: 100 },
+
+  { nome: "camiseta", segmento: "vestuario", preco: 249, estoque: 100 },
+  { nome: "bandeira", segmento: "vestuario", preco: 100, estoque: 100 },
+  { nome: "short ", segmento: "vestuario", preco: 59, estoque: 100 },
+  { nome: "tenis ", segmento: "vestuario", preco: 300, estoque: 100 },
+  { nome: "chinelo ", segmento: "vestuario", preco: 39, estoque: 100 },
+  { nome: "camiseta regata ", segmento: "vestuario", preco: 99, estoque: 100 },
+  { nome: "calça ", segmento: "vestuario", preco: 199, estoque: 100 },
+  { nome: "boné ", segmento: "vestuario", preco: 79, estoque: 100 },
+  { nome: "cinto ", segmento: "vestuario", preco: 89, estoque: 100 },
+  { nome: "sunga ", segmento: "vestuario", preco: 69, estoque: 100 },
+];
+
 var carrinho = [];
 
 console.log("INCIALIZOU");
 
 inicializarLocalStorage();
 carregarArraysComLocalStorage();
-// localStorage.setItem("usuarios", JSON.stringify(usuarios));
-// localStorage.setItem("marcas", JSON.stringify(marcas));
-// localStorage.setItem("produtos", JSON.stringify(produtos));
-// localStorage.setItem("carrinho", JSON.stringify(carrinho));
-
-function cadastrarSegmento() {
-  console.log("teste");
-  let nomeSegmentoInformado = document.getElementById(
-    "input-segmento-nome"
-  ).value;
-
-  s = {
-    nome: nomeSegmentoInformado,
-  };
-
-  segmentos.push(s);
-  localStorage.setItem("segmentos", JSON.stringify(segmentos));
-}
 
 function existeSegmento() {}
 function validarSegmento() {}
 function excluirSegmento() {}
-function atualizarTabelaSegmentos() {}
-function cadastrarProduto() {}
-function validarProduto() {}
-function existeProduto() {}
-function excluirProduto() {}
-function atualizarTabelaProdutos() {}
+
+function atualizarTabelaSegmentos() {
+  var dados = "";
+  for (s of segmentos) {
+    dados += `<tr>
+                <td>${s.nome}</td>
+                <td><button onclick="excluirMarca(${segmentos.indexOf(
+                  s
+                )})" class="btn btn-outline-danger btn-sm">Excluir</button></td>
+                </tr>`;
+  }
+
+  document.getElementsByTagName("tbody")[0].innerHTML = dados;
+}
+
+function atualizarTabelaProdutos() {
+  console.log("rodou");
+  var dados = "";
+  for (p of produtos) {
+    dados += `<tr>
+                <td>${p.nome}</td>
+                <td>${p.segmento}</td>
+                <td>${p.estoque}</td>
+                <td>R$ ${p.preco}</td>
+                <td><button onclick="excluirMarca(${produtos.indexOf(
+                  p
+                )})" class="btn btn-outline-danger btn-sm">Excluir</button></td>
+                </tr>`;
+
+    document.getElementsByTagName("tbody")[0].innerHTML = dados;
+  }
+}
 
 function carregarArraysComLocalStorage() {
   if (localStorage.getItem("segmentos") != null) {
@@ -50,15 +86,18 @@ function carregarArraysComLocalStorage() {
 }
 
 function inicializarLocalStorage() {
-  if (localStorage.getItem("segmentos") == null) {
-    localStorage.setItem("segmentos", JSON.stringify(segmentos));
-  }
+  localStorage.setItem("segmentos", JSON.stringify(segmentos));
+  localStorage.setItem("produtos", JSON.stringify(produtos));
+  localStorage.setItem("carrinho", JSON.stringify(carrinho));
+  // if (localStorage.getItem("segmentos") == null) {
+  // localStorage.setItem("segmentos", JSON.stringify(segmentos));
+  // }
 
-  if (localStorage.getItem("produtos") == null) {
-    localStorage.setItem("produtos", JSON.stringify(produtos));
-  }
+  // if (localStorage.getItem("produtos") == null) {
+  // localStorage.setItem("produtos", JSON.stringify(produtos));
+  // }
 
-  if (localStorage.getItem("carrinho") == null) {
-    localStorage.setItem("carrinho", JSON.stringify(carrinho));
-  }
+  // if (localStorage.getItem("carrinho") == null) {
+  // localStorage.setItem("carrinho", JSON.stringify(carrinho));
+  // }
 }
